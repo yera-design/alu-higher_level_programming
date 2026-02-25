a4d0d199cd8d92d61787c9409167899d66f164e7
yera-design <p.yera@alustudent.com>
2026-02-25 22:56:33 +0200

    corrected

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print(f"{argc} arguments:")
-    
     for i in range(1, len(sys.argv)):
         print(f"{i}: {sys.argv[i]}")
