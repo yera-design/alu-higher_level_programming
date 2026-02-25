d18e727a186177488a5e681278871d3a42bbd6f8
yera-design <p.yera@alustudent.com>
2026-02-25 13:02:26 +0200

    created all scripts

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    import hidden_4
+    
+    names = dir(hidden_4)
+    
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+This repository contain various scripts that are mainly about Python-import & modules
