faf2f3d44569ac0dbead2c1a1e54b98cf2b3eef9
yera-design <p.yera@alustudent.com>
2026-02-25 22:53:36 +0200

    modified scripts

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,6 @@
 import sys
 if __name__ == "__main__":
     argc = len(sys.argv) - 1  
-    
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,7 @@
 import sys
 if __name__ == "__main__":
     import hidden_4
-    
     names = dir(hidden_4)
-    
     for name in sorted(names):
         if not name.startswith('__'):
             print(name)
