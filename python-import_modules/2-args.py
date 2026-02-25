a12ae23a0c6c1dc40b261edb53f544089ee63c7a
yera-design <p.yera@alustudent.com>
2026-02-25 23:00:39 +0200

    corrected

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
+
 if __name__ == "__main__":
-    argc = len(sys.argv) - 1  
+    argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
     elif argc == 1:
