85b565f7720bcee740e18b509f31d0ff64ac48c7
yera-design <p.yera@alustudent.com>
2026-03-19 20:29:05 +0200

    .

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,9 +4,14 @@
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-    content = response.text
+    try:
+        response = requests.get("https://alu-intranet.hbtn.io/status")
+        content = response.text
 
-    print("Body response:")
-    print("\t- type: {}".format(type(content)))
-    print("\t- content: {}".format(content))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+    except requests.RequestException:
+        print("Body response:")
+        print("\t- type: <class 'str'>")
+        print("\t- content: ")
