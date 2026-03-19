869f4d4a27177d431c81c226f97b6990475a3da3
yera-design <p.yera@alustudent.com>
2026-03-19 20:13:08 +0200

    some changes made

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-documenting strings
-"""
 import requests
 
-
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    if url.startswith('https://'):
-        url = "https://alu-intranet.hbtn.io/status"
-    res = requests.get(url)
+    response = requests.get("https://alu-intranet.hbtn.io/status")
+    content = response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
